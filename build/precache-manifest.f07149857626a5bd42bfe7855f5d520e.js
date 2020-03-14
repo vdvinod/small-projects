e1545bfd26865aac7d7b19156b5ae5f262da36aa
vinod singh rawat <vdvinodrawat@gmail.com>
2020-03-14 17:57:45 +0530

    portfoliio

---
+++ build/precache-manifest.f07149857626a5bd42bfe7855f5d520e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26a7dab6a54c79b163abd96f65d95670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3cd02ace00aee04481e",
+    "url": "/static/css/main.e067941b.chunk.css"
+  },
+  {
+    "revision": "0f4d6c823240e55c61ff",
+    "url": "/static/js/2.7245b878.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.7245b878.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3cd02ace00aee04481e",
+    "url": "/static/js/main.52286393.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
