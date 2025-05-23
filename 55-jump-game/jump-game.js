b4171b2623e4139b69387cd1049fff19c0a4c23a
Vinod Singh Rawat <vdvinodrawat@gmail.com>
2025-05-23 15:24:56 -0400

    Time: 1 ms (83.92%) | Memory: 59.1 MB (45.13%) - LeetSync

---
+++ 55-jump-game/jump-game.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let maxReach = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (i > maxReach) return false;
+        maxReach = Math.max(maxReach, i + nums[i]);
+    }
+    
+    return true;
+};
