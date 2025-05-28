9c53c4124a216bf16e034f52fa559346ce257fd8
Vinod Singh Rawat <vdvinodrawat@gmail.com>
2025-05-28 16:52:50 -0400

    Time: 32 ms (50.04%) | Memory: 85.6 MB (24.50%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+     const map = new Map(); // value -> index
+
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i]) && i - map.get(nums[i]) <= k) {
+      return true;
+    }
+
+    map.set(nums[i], i); // update the latest index
+  }
+
+  return false;
+};
