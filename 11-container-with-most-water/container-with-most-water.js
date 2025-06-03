a80d6cb7fdd24885f3309ec7108a0d02e17e979a
Vinod Singh Rawat <vdvinodrawat@gmail.com>
2025-06-02 20:30:01 -0400

    Time: 0 ms (100.00%) | Memory: 62.6 MB (63.12%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.js
+++ 11-container-with-most-water/container-with-most-water.js
@@ -2,19 +2,38 @@
  * @param {number[]} height
  * @return {number}
  */
-var maxArea = function(height) {
-    left = 0;
-    right = height.length - 1;
-    maxWater = 0;
-    while(left < right){
-        maxheight = Math.min(height[left], height[right]);
-        gap = right - left;
-        maxWater = Math.max(maxWater, maxheight * gap)
-        if(height[left] < height[right]){
-            left++
-        }else{
-            right--
-        }
+// var maxArea = function(height) {
+//     left = 0;
+//     right = height.length - 1;
+//     maxWater = 0;
+//     while(left < right){
+//         maxheight = Math.min(height[left], height[right]);
+//         gap = right - left;
+//         maxWater = Math.max(maxWater, maxheight * gap)
+//         if(height[left] < height[right]){
+//             left++
+//         }else{
+//             right--
+//         }
+//     }
+//     return maxWater;
+// };
+function maxArea(height) {
+  let left = 0;
+  let right = height.length - 1;
+  let maxWater = 0;
+
+  while (left < right) {
+    const width = right - left;
+    const area = width * Math.min(height[left], height[right]);
+    maxWater = Math.max(maxWater, area);
+
+    if (height[left] < height[right]) {
+      left++; // Try to find a taller line
+    } else {
+      right--; // Try to find a taller line
     }
-    return maxWater;
-};
+  }
+
+  return maxWater;
+}
