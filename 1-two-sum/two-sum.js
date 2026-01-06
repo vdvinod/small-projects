f903b044e4e7fee379e38bc6afbea21a08dca3da
Vinod Singh Rawat <vdvinodrawat@gmail.com>
2026-01-06 16:23:53 -0500

    Time: 0 ms (100.00%) | Memory: 54.5 MB (85.80%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,15 +4,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
- const map = new Map();
+ const map = new Map()
 
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i];
-
-    if (map.has(complement)) {
-      return [map.get(complement), i];
+for(let i=0; i<= nums.length; i++){
+    let a = target - nums[i];
+    if(map.has(a)) {
+        return [map.get(a), i]
     }
-
-    map.set(nums[i], i);
-  }
+    map.set(nums[i], i)
+}
 };
+
